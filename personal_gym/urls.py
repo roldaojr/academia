c64a32df238a3ff74e4f4b22985d1b68b77f3e34
Roldão Rego Jr <roldaogjr@gmail.com>
2017-03-14 21:02:48 +0000

    Logout redireciona pra login

--- personal_gym/urls.py
+++ personal_gym/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout_then_login
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'login', login, name='login'),
-    url(r'logout', logout, name='logout'),
+    url(r'logout', logout_then_login, name='logout'),
     url(r'', include('academia.urls'))
 ]
