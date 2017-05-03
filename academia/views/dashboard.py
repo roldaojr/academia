243bee5a09a25b5f485f5fab73ee3060c26de23d
Cristiano Nunes <cristianonunes99@gmail.com>
2017-05-03 00:18:34 +0000

    Dashboard concluída

--- academia/views/dashboard.py
+++ academia/views/dashboard.py
@@ -5,22 +5,30 @@ from django.contrib.auth.decorators import login_required
 from django.utils.timezone import *
 from datetime import *
 from ..models import Treino
+from ..models import AvaliacaoFisica
 
 
 
 @login_required
 def treinoexpirado(request):
+    
+    #treino expirado
     treinos = Treino.objects.filter(data_fim__lte=now())
+    
+    #treino expirando
+    hoje = date.today()
+    amanha = (date.today() + timedelta(days=1))
+    futuro = (hoje + timedelta(days=7))
+    texpirando = Treino.objects.filter(data_fim__gte=amanha, data_fim__lte=futuro)
+    
+    #avaliação física expirada
+    avaliacoes = AvaliacaoFisica.objects.filter(data_refazer__lte=now())
+    
+    #avaliação física expirando
+    avaliacaoexpirando = AvaliacaoFisica.objects.filter(data_refazer__gte=amanha, data_refazer__lte=futuro)
+    
     return render(request, 'dashboard.html', {
-        'treinos':treinos
+        'treinos':treinos, 'texpirando':texpirando, 'avaliacoes':avaliacoes, 'avaliacaoexpirando':avaliacaoexpirando
     })
    
-    
-@login_required
-def treinoexpirando(request):
-    hoje = date.today()
-    futuro = (hoje - datetime.timedelta(days=7))
-    texpirando = Treino.objects.filter(data_fim__gte=futuro)
-    return render(request, 'dashboard.html', {
-        'texpirando':texpirando
-    })
+   
